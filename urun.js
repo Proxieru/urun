9b1aefa8918486e2fb54acb68ae1ec97498553a9
Proxieru <165452030+Proxieru@users.noreply.github.com>
2024-04-30 21:15:58 -0700

    Update urun.js

--- urun.js
+++ urun.js
@@ -1,14 +1,7 @@
 /// urun.js
 document.addEventListener("keydown", function (e) {
 	if (e.key == "~" && e.ctrlKey) {
-		var t = window.open("", "_blank", "width=500,height=300");
-		var e = t.document.createElement("iframe");
-		(e.src = "https://raw.githubusercontent.com/Proxieru/urun/main/test.js"),
-			(e.style.cssText = "width:100%; height:100%; border:none;"),
-			t.document.body.appendChild(e),
-			t.document.title = "uRun",
-			t.addEventListener("message", function (e) {
-				e.data.toString().startsWith("execute:") && (eval(e.data.toString().replace("execute:", "")), t.close());
+		(e.src = "https://raw.githubusercontent.com/Proxieru/urun/main/test.js")
 			});
 	}
 });
