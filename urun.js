e20c0fe545b1fb3980171c2a46a34996630a2917
Proxieru <165452030+Proxieru@users.noreply.github.com>
2024-04-30 21:44:21 -0700

    Delete urun.js

--- urun.js
+++
@@ -1,7 +0,0 @@
-/// urun.js
-document.addEventListener("keydown", function (e) {
-	if (e.key == "~" && e.ctrlKey) {
-		(e.src = "https://raw.githubusercontent.com/Proxieru/urun/main/test.js")
-			});
-	}
-});
